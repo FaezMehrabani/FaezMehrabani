6727a321e38d82bcd65abb4a94c4dd5e852b20d6
FaezMehrabani <f.mehrabani1986@gmail.com>
2015-01-12 13:13:15 +0330

    add all files

---
+++ gh-pages/php/config.php
@@ -0,0 +1,14 @@
+<?
+	//define the receiver of the email
+	define('TO_EMAIL','f.mehrabani1986@gmail.com');
+	
+	//define the subject of the email
+	define('SUBJECT','From F-Mehrabani');	
+	
+	// Messages
+	define('MSG_INVALID_NAME','لطفا نام را وارد نمائید.');
+	define('MSG_INVALID_EMAIL','لطفا ایمیل معتبر وارد نمائید.');
+	define('MSG_INVALID_MESSAGE','لطفا متن پیام را وراد کنید.');
+	define('MSG_SEND_ERROR','خطا در ارسال ایمیل...');
+
+?>
